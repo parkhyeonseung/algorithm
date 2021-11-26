def7df2b21b2f0ebf503dbe725d5f301ebc56fda
parkheonseung <gus0tmd@naver.com>
2021-11-26 10:57:20 +0900

    0

---
+++ valuesize01.py
@@ -0,0 +1,19 @@
+second = 50
+
+def func1(first01):
+    first = 10
+    func2(first01)
+    return
+
+def func2(first02):
+    first = 20
+    return
+
+
+if __name__ == '__main__':
+    # first = 30
+    # func1(first)
+    a = 30 
+    b = 4.2
+    c = 'G'
+    pass
